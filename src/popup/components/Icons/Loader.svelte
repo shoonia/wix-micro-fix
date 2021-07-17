7bc9383aebfa0841366a1efad788d835d2e65b99
shoonia <alexanderz@wix.com>
2021-07-18 00:56:08 +0300

    Optimization of svg icons

--- src/popup/components/Icons/Loader.svelte
+++ src/popup/components/Icons/Loader.svelte
@@ -1,60 +1,17 @@
-<svg
-  width="99%"
-  height="99%"
-  viewBox="0 0 250 215"
->
-  <rect
-    x="0"
-    y="0"
-    width="100%"
-    height="100%"
-    clip-path="url(#clip-path)"
-    fill="url(#fill)"
-  />
-  <defs>
-    <clipPath id="clip-path">
-      <rect x="28" y="80" rx="0" ry="0" width="200" height="50"/>
-    </clipPath>
-    <linearGradient id="fill">
-      <stop
-        offset="0.6"
-        stop-color="rgba(255,255,255,0.5)"
-        stop-opacity="1"
-      >
-        <animate
-          attributeName="offset"
-          values="-2; -2; 1"
-          keyTimes="0; 0.25; 1"
-          dur="2s"
-          repeatCount="indefinite"
-        />
-      </stop>
-      <stop
-        offset="1.5"
-        stop-color="#dfe5eb"
-        stop-opacity="1"
-      >
-        <animate
-          attributeName="offset"
-          values="-1; -1; 2"
-          keyTimes="0; 0.25; 1"
-          dur="1s"
-          repeatCount="indefinite"
-        />
-      </stop>
-      <stop
-        offset="2.6"
-        stop-color="#dfe5eb"
-        stop-opacity="1"
-      >
-        <animate
-          attributeName="offset"
-          values="0; 0; 3"
-          keyTimes="0; 0.25; 1"
-          dur="1s"
-          repeatCount="indefinite"
-        />
-      </stop>
-    </linearGradient>
-  </defs>
+<svg width="99%" height="99%" viewBox="0 0 250 215">
+  <rect width="100%" height="100%" fill="url(#fill)" clip-path="url(#clip-path)"/>
+  <linearGradient id="fill">
+    <stop offset=".6" stop-color="rgba(255,255,255,.5)">
+      <animate attributeName="offset" dur="2s" keyTimes="0;0.25;1" repeatCount="indefinite" values="-2;-2;1"/>
+    </stop>
+    <stop offset="1.5" stop-color="#dfe5eb">
+      <animate attributeName="offset" dur="1s" keyTimes="0;0.25;1" repeatCount="indefinite" values="-1;-1;2"/>
+    </stop>
+    <stop offset="2.6" stop-color="#dfe5eb">
+      <animate attributeName="offset" dur="1s" keyTimes="0;0.25;1" repeatCount="indefinite" values="0;0;3"/>
+    </stop>
+  </linearGradient>
+  <clipPath id="clip-path">
+    <rect width="200" height="50" x="28" y="80" rx="0" ry="0"/>
+  </clipPath>
 </svg>
